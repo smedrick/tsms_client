88b144c9041b84a0dc7e6c2f911c6d587bd4009e
Billy Reisinger <billy.reisinger@govdelivery.com>
2012-10-24 12:08:38 -0500

    GOVD-3553 whoops, forgot to change spec name

--- spec/tsms_client_spec.rb
+++ spec/tsms_client_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe TsmsClient, "version" do
-  it "returns 0 for all gutter game" do
+  it "should exist" do
     TsmsClient::VERSION.should be_an_instance_of(String)
   end
 end
