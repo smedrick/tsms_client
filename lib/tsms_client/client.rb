e3171208c01a8713fc264488abadddb3cc06c27c
brianroth <briaroth@gmail.com>
2012-11-08 13:14:49 -0600

    Use api_url when posting messages

--- lib/tsms_client/client.rb
+++ lib/tsms_client/client.rb
@@ -3,15 +3,15 @@ class TSMS::Client
   include TSMS::CoreExt
 
   attr_accessor :connection, :href
-  DEFAULT_ENDPOINT = 'http://localhost:3000'
 
-  def initialize(username, password, api_root = DEFAULT_ENDPOINT)
-    connect!(username, password, api_root)
+  def initialize(username, password, api_root = 'http://localhost:3000')
+    @api_root = api_root
+    connect!(username, password)
     discover!
   end
 
-  def connect!(username, password, api_root)
-    self.connection = TSMS::Connection.new(:username => username, :password => password, :api_root => api_root)
+  def connect!(username, password)
+    self.connection = TSMS::Connection.new(:username => username, :password => password, :api_root => @api_root)
   end
 
   def discover!
@@ -25,7 +25,7 @@ class TSMS::Client
 
   def post(obj)
     raw_connection.post do |req|
-      req.url DEFAULT_ENDPOINT + obj.href
+      req.url @api_root + obj.href
       req.headers['Content-Type'] = 'application/json'
       req.body = obj.to_json(true)
     end
